9ee2688eae7e1d0038a69ca34c1313252fc23cc2
sbr-vincent <vincent.g.sbr@gmail.com>
2021-10-29 11:14:49 -0700

    Cut some code

--- rottenOranges.js
+++ rottenOranges.js
@@ -3,6 +3,7 @@
  * @return {number}
  */
 var orangesRotting = function (grid) {
+    debugger;
     let fresh = 0;
     let rot = 0;
     let minutes = 0;
@@ -11,26 +12,7 @@ var orangesRotting = function (grid) {
 
         for (let j = 0; j < grid[i].length; j++) {
             if (grid[i][j] == 1) {
-                let alwaysFresh = true;
-                if (grid[i - 1] != undefined) {
-                    if (grid[i - 1][j] == 2 || grid[i - 1][j] == 1) {
-                        alwaysFresh = false;
-                    }
-                }
-                if (grid[i + 1] != undefined) {
-                    if (grid[i + 1][j] == 2 || grid[i + 1][j] == 1) {
-                        alwaysFresh = false;
-                    }
-                }
-                if ((grid[i][j - 1] != 0 && grid[i][j - 1] != undefined) ||
-                    (grid[i][j + 1] != 0 && grid[i][j + 1] != undefined)) {
-                    alwaysFresh = false;
-                }
-                if (alwaysFresh) {
-                    return -1;
-                }
                 fresh++;
-
             }
             if (grid[i][j] == 2) {
                 rot++;
