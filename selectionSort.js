9ad3583ff42ac106c1a3e047f5c3158160bdc626
sbr-vincent <vincent.g.sbr@gmail.com>
2023-02-07 08:15:58 -0800

    Selection Sort

---
+++ selectionSort.js
@@ -0,0 +1,66 @@
+/* 
+  https://visualgo.net/en/sorting
+    
+  Selection sort works by iterating through the list, finding the minimum
+  value, and moving it to the beginning of the list. Then, ignoring the first
+  position, which is now sorted, iterate through the list again to find the
+  next minimum value and move it to index 1. This continues until all values
+  are sorted.
+  Unstable sort.
+  
+  Time Complexity
+    - Best: O(n^2) quadratic.
+    - Average: O(n^2) quadratic.
+    - Worst: O(n^2) quadratic.
+  Space: O(1) constant.
+  Selection sort is one of the slower sorts.
+  - ideal for: pagination, where page 1 displays 10 records for example,
+      you run selection sort for 10 iterations only to display the first 10
+      sorted items.
+*/
+
+const numsOrdered = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const numsRandomOrder = [9, 2, 5, 6, 4, 3, 7, 10, 1, 8];
+const numsReversed = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1];
+const expected = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+/**
+ * Sorts given array in-place.
+ * Best: O(n^2) quadratic.
+ * Average: O(n^2) quadratic.
+ * Worst: O(n^2) quadratic.
+ * @param {Array<number>} nums
+ * @returns {Array<number>} The given array after being sorted.
+ */
+function selectionSort(nums = []) {
+    let sortedArr = [...nums];
+    let min = Number.MAX_SAFE_INTEGER;
+    let minIndex = 0;
+
+    for (let i = 0; i < sortedArr.length; i++) {
+        for (let j = i + 1; j < sortedArr.length; j++) {
+            if (sortedArr[j] < min) {
+                minIndex = j;
+                min = Math.min(min, sortedArr[j]);
+            }
+        }
+
+        if (min < sortedArr[i]) {
+            // temp = sortedArr[i]
+            // sortedArr[i] = sortedArr[minIndex]
+            // sortedArr[minIndex] = temp
+            [sortedArr[i], sortedArr[minIndex]] = [
+                sortedArr[minIndex],
+                sortedArr[i],
+            ];
+        }
+
+        min = Number.MAX_SAFE_INTEGER;
+    }
+
+    return sortedArr;
+}
+
+console.log(selectionSort(numsOrdered));
+console.log(selectionSort(numsRandomOrder));
+console.log(selectionSort(numsReversed));
