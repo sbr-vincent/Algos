cf5fbf8eb97d10d3fc7b2800201a1f98440151fd
sbr-vincent <vincent.g.sbr@gmail.com>
2023-05-09 07:37:40 -0700

    Optimized Two Sum

--- twoSum.js
+++ twoSum.js
@@ -17,3 +17,21 @@ var twoSum = function(nums, target) {
     
     return ansArray;
 };
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    let numsToFind = {}
+
+    for(let i = 0; i < nums.length;i++){
+        let targetNum = target - nums[i]
+        if(numsToFind[nums[i]] != undefined){
+            return [i, numsToFind[nums[i]]]
+        }else{
+            numsToFind[targetNum] = i
+        }
+    }
+};
