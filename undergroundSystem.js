0e905c16d9ec8622676484c47e09d2a3c9ed5f90
sbr-vincent <vincent.g.sbr@gmail.com>
2023-05-31 08:30:23 -0700

    1396 Design Underground

---
+++ undergroundSystem.js
@@ -0,0 +1,49 @@
+var UndergroundSystem = function() {
+    this.avg = new Map();
+    this.train = new Map();
+};
+
+/** 
+ * @param {number} id 
+ * @param {string} stationName 
+ * @param {number} t
+ * @return {void}
+ */
+UndergroundSystem.prototype.checkIn = function(id, start, t) {
+    this.train.set(id, [start, t]);
+};
+
+/** 
+ * @param {number} id 
+ * @param {string} stationName 
+ * @param {number} t
+ * @return {void}
+ */
+UndergroundSystem.prototype.checkOut = function(id, end, t) {
+    const [start, s] = this.train.get(id);
+    const key = [start, end].join();
+    if (this.avg.has(key)) {
+        let [avg, cnt] = this.avg.get(key);
+        this.avg.set(key, [avg * (cnt/++cnt) + ((t - s)/cnt), cnt]);
+    } else {
+        this.avg.set(key, [(t - s), 1]);
+    }
+    this.train.delete(id);
+};
+
+/** 
+ * @param {string} startStation 
+ * @param {string} endStation
+ * @return {number}
+ */
+UndergroundSystem.prototype.getAverageTime = function(start, end) {
+    return this.avg.get([start, end].join())[0];
+};
+
+/** 
+ * Your UndergroundSystem object will be instantiated and called as such:
+ * var obj = new UndergroundSystem()
+ * obj.checkIn(id,stationName,t)
+ * obj.checkOut(id,stationName,t)
+ * var param_3 = obj.getAverageTime(startStation,endStation)
+ */
