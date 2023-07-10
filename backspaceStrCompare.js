eb73b40b0d8dd9e789df2d3bc2f71e92b998196b
sbr-vincent <vincent.g.sbr@gmail.com>
2023-07-10 12:55:47 -0700

    Added optimized solution

--- backspaceStrCompare.js
+++ backspaceStrCompare.js
@@ -34,4 +34,47 @@ var buildString = function(string){
     return newArray
 }
 
-// Optimized
+// Optimized Time: O(n+m) Space: O(1)
+var backspaceCompare = function(s, t) {
+    let sPointer = s.length-1, tPointer = t.length-1
+    let count = 0
+    while(sPointer >= 0 || tPointer >= 0){
+        if(s[sPointer] === '#'){
+            count++;
+            sPointer--;
+            while(count > 0){
+                if(s[sPointer] === '#'){
+                    count++;
+                    sPointer--;
+                }else{
+                    count--
+                    sPointer--;
+                }
+            }
+            
+            count = 0
+        }else if(t[tPointer] === '#'){
+            count++
+            tPointer--;
+            while(count > 0){
+                if(t[tPointer] === '#'){
+                    count++;
+                    tPointer--;
+                }else{
+                    count--
+                    tPointer--;
+                }
+            }
+            
+            count = 0
+        }else if(s[sPointer] === t[tPointer]){
+            sPointer--;
+            tPointer--;
+            continue
+        }else{
+            return false
+        }
+    }
+
+    return true
+};
