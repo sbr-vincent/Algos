b19102a59c86b057867b7b7f0e88ff384cee7a12
sbr-vincent <vincent.g.sbr@gmail.com>
2023-12-21 15:50:19 -0800

    Merge Sort solution

---
+++ mergeSort.js
@@ -0,0 +1,35 @@
+const numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
+
+function mergeSort (array) {
+  if (array.length === 1) {
+    return array
+  }
+  // Split Array in into right and left
+  let left = array.slice(0, (Math.floor(array.length/2)));
+  let right = array.slice((Math.floor(array.length/2)))
+  return merge(
+    mergeSort(left),
+    mergeSort(right)
+  )
+}
+
+function merge(left, right){
+    let sortedArry = []
+    let i = 0, j = 0;
+
+    while(i < left.length && j < right.length){
+        if(left[i] < right[j]){
+            sortedArry.push(left[i])
+            i++
+        }else{
+            sortedArry.push(right[j])
+            j++
+        }
+    }  
+
+    return sortedArry.concat(left.slice(i)).concat(right.slice(j));
+}
+
+
+const answer = mergeSort(numbers);
+console.log(answer);
