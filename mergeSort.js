485b5b00c5276de65062869a31062a81cca3259b
sbr-vincent <vincent.g.sbr@gmail.com>
2023-12-21 15:55:08 -0800

    Added comments

--- mergeSort.js
+++ mergeSort.js
@@ -7,6 +7,8 @@ function mergeSort (array) {
   // Split Array in into right and left
   let left = array.slice(0, (Math.floor(array.length/2)));
   let right = array.slice((Math.floor(array.length/2)))
+
+  // Keep splitting the left and right sides until we have multiple arrays with one value
   return merge(
     mergeSort(left),
     mergeSort(right)
@@ -17,6 +19,7 @@ function merge(left, right){
     let sortedArry = []
     let i = 0, j = 0;
 
+    // Go through the arrays and push the smaller value into sortedArray. Stop the loop once we reach the end of one of the arrays
     while(i < left.length && j < right.length){
         if(left[i] < right[j]){
             sortedArry.push(left[i])
@@ -27,6 +30,7 @@ function merge(left, right){
         }
     }  
 
+    // Concat is used to attach the remaining value that we didn't get to in the while loop.
     return sortedArry.concat(left.slice(i)).concat(right.slice(j));
 }
 
